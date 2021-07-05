384c1d72ee8689daf7d5c096887b8f9d10b41e11
Clif Wolfe <clifwolfe1@gmail.com>
2021-07-05 17:05:58 -0400

    removed more dead code

--- elfparser/__init__.py
+++ elfparser/__init__.py
@@ -61,24 +61,3 @@ from . import elfstructs
 from . import constexpr
 
 
-def instantiate_ctype_with_backing(classtype, backing=None):
-    """Create an instance of the given type using the backing provided, or if
-    no backing was provided create a backing as well"""
-    if backing is None:
-        backing = ARRAY(c_ubyte, sizeof(classtype))()
-    else:
-        if len(backing) != sizeof(classtype):
-            raise Exception("Provided backing and classtype have different sizes")
-    ptr = cast(backing, POINTER(classtype))
-    ptr.contents._elfparser_backing = backing
-
-    return backing, ptr.contents
-
-
-def set_backing_value(backing, value):
-    backing[:] = value
-
-
-def set_backing_value_from_elf_offset(backing, elf, offset):
-    set_backing_value(backing, elf[offset:offset + len(backing)])
-
